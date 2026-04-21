1bacaa6d2f32ce3310fada7cabb65c4c631f21c2
Ethan Chien <liangtie.qian@gmail.com>
2026-04-22 01:48:21 +0800

    refactor(kicad-parser): update exports to use wildcard syntax
    - Replace individual named exports with wildcard exports for better
      module organization
    - Add export for schematic_serializer module
    - Update import syntax to consistent double quotes

--- packages/kicad-parser/src/index.ts
+++ packages/kicad-parser/src/index.ts
@@ -1,7 +1,8 @@
-export { BoardParser } from './board_parser';
-export { SchematicParser } from './schematic_parser';
-export { parse_drawing_sheet } from './drawing_sheet_parser';
-export * as boardProto from './proto/board';
-export * as schematicProto from './proto/schematic';
-export * as drawingSheetProto from './proto/drawing-sheet';
-export * as commonProto from './proto/common';
+export * from "./board_parser";
+export * from "./schematic_parser";
+export * from "./schematic_serializer";
+export * from "./drawing_sheet_parser";
+export * as boardProto from "./proto/board";
+export * as schematicProto from "./proto/schematic";
+export * as drawingSheetProto from "./proto/drawing-sheet";
+export * as commonProto from "./proto/common";
